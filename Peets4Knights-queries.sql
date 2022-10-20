3982bf39d9a6687dd9837183bd5f40f6d4181497
Matt Van Harn <mattvanharn@gmail.com>
2022-10-20 16:15:59 -0400

    Create Peets4Knights.sql and Peets4Knights-queries.sql files

---
+++ Peets4Knights-queries.sql
@@ -0,0 +1,12 @@
+-- This SQL script implements sample queries on the Peets4Knights database.
+
+-- @author mattvanharn
+-- @version Fall, 2022
+
+-- Get the number of DrinkMenu records
+
+-- Get the number of FoodMenu records
+
+-- Get the DrinkSpecification records
+
+-- Get the order history records
---
+++ Peets4Knights.sql
@@ -0,0 +1,13 @@
+-- This SQL script builds a Peets4Knights database, deleting any pre-existing version.
+--
+-- @author mattvanharn
+-- @version Fall, 2022
+
+
+-- Drop previous versions of the tables if they exsts, in reverse order of the foreign keys.
+
+-- Create the schema.
+
+-- Allow users to select data from the tables.
+
+-- Add sample records.
